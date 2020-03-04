c6de8f44bd5ab8b0a0bbf321bc105e5a511f2222
Ed Prince <edward_prince@hotmail.com>
2020-03-04 23:43:37 +0000

    service working registered, pwa conversion

---
+++ build/precache-manifest.c94b8976a58d29dc42ff1a145faae4d8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5f5c43447b9ad0c6b073c58a1f12bb26",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bee2add47504c6dbf289",
+    "url": "/static/css/main.2afa6b53.chunk.css"
+  },
+  {
+    "revision": "bd033b5af98f3bf135db",
+    "url": "/static/js/2.3bcf4626.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/static/js/2.3bcf4626.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bee2add47504c6dbf289",
+    "url": "/static/js/main.8faa728d.chunk.js"
+  },
+  {
+    "revision": "ff8921054224e0da5298",
+    "url": "/static/js/runtime-main.b1fd2240.js"
+  }
+]);
